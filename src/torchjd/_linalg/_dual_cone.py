cf0439cf40ddcfb01950d9669888af747dab50ca
Valérian Rey <valerian.rey@gmail.com>
2026-05-13 19:48:45 +0200

    Fix formatting of maths in docstrings of projectors

--- src/torchjd/_linalg/_dual_cone.py
+++ src/torchjd/_linalg/_dual_cone.py
@@ -19,21 +19,27 @@ class DualConeProjector(ABC):
     @abstractmethod
     def __call__(self, U: Tensor, G: PSDMatrix) -> Tensor:
         r"""
-        Computes the weights `w` of the projection of `J^T u` onto the dual cone of
-        the rows of `J`, provided `G = J J^T` and `u`. In other words, this computes the `w` that
-        satisfies `\pi_J(J^T u) = J^T w`, with `\pi_J` defined in Equation 3 of [1].
+        Computes the weights :math:`w` of the projection of :math:`J^\top u` onto the dual cone of
+        the rows of :math:`J`, provided :math:`G = J J^\top` and :math:`u`. In other words, this
+        computes the :math:`w` that satisfies :math:`\pi_J(J^\top u) = J^\top w`, with
+        :math:`\pi_J` defined in Equation 3 of [1].
 
-        By Proposition 1 of [1], this is equivalent to solving for `v` the following quadratic
-        program:
-        minimize        v^T G v
-        subject to      u \preceq v
+        By Proposition 1 of [1], this is equivalent to solving for :math:`v` the following
+        quadratic program:
+
+        .. math::
+
+            \min_{v} \quad & v^\top G v \\
+            \text{subject to} \quad & u \preceq v
 
         Reference:
         [1] `Jacobian Descent For Multi-Objective Optimization <https://arxiv.org/pdf/2406.16232>`_.
 
-        :param U: The tensor of weights corresponding to the vectors to project, of shape `[..., m]`.
-        :param G: The Gramian matrix of shape `[m, m]`. It must be symmetric and positive definite.
-        :return: A tensor of projection weights with the same shape as `U`.
+        :param U: The tensor of weights corresponding to the vectors to project, of shape
+            ``[..., m]``.
+        :param G: The Gramian matrix of shape ``[m, m]``. It must be symmetric and positive
+            definite.
+        :return: A tensor of projection weights with the same shape as ``U``.
         """
 
 
