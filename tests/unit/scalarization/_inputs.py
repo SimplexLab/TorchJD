1693fb4b1a206b739728ab2cf70005bbab8bc61e
ppraneth <pranethparuchuri@gmail.com>
2026-05-28 13:35:15 +0530

    fix _inputs.py
    Signed-off-by: ppraneth <pranethparuchuri@gmail.com>

--- tests/unit/scalarization/_inputs.py
+++ tests/unit/scalarization/_inputs.py
@@ -1,9 +1,5 @@
 from torch import Tensor
-from utils.tensors import randn_, tensor_
+from utils.tensors import randn_
 
-scalar_input: Tensor = tensor_(7.0)
-vector_input: Tensor = randn_(5)
-matrix_input: Tensor = randn_(3, 4)
-tensor_3d_input: Tensor = randn_(2, 3, 4)
-
-all_inputs: list[Tensor] = [scalar_input, vector_input, matrix_input, tensor_3d_input]
+shapes: list[list[int]] = [[], [5], [3, 4], [2, 3, 4]]
+all_inputs: list[Tensor] = [randn_(shape) for shape in shapes]
