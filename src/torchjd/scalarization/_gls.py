5966b0dfc04185ef9c6549c5d2edf7d6b7f9c77c
Praneth Paruchuri <pranethparuchuri@gmail.com>
2026-05-29 13:41:22 +0530

    Update src/torchjd/scalarization/_gls.py
    Co-authored-by: Pierre Quinton <pierre.quinton@gmail.com>

--- src/torchjd/scalarization/_gls.py
+++ src/torchjd/scalarization/_gls.py
@@ -7,7 +7,7 @@ from ._scalarizer_base import Scalarizer
 class GLS(Scalarizer):
     """
     :class:`~torchjd.scalarization.Scalarizer` that returns the geometric mean of the input tensor
-    of values, as defined in `MultiNet++: Multi-Stream Feature Aggregation and Geometric Loss
+    of values, as studied in `MultiNet++: Multi-Stream Feature Aggregation and Geometric Loss
     Strategy for Multi-Task Learning
     <https://openaccess.thecvf.com/content_CVPRW_2019/papers/WAD/Chennupati_MultiNet_Multi-Stream_Feature_Aggregation_and_Geometric_Loss_Strategy_for_Multi-Task_CVPRW_2019_paper.pdf>`_.
     """
